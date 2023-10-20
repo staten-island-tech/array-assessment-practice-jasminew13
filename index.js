b98cee9322b6211b148f877f56f5a701c40f2725
jasminew13 <jasminewu101@gmail.com>
2023-10-20 10:15:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -88,15 +88,35 @@ const titles = [
 //"--- wrote --- in ---"
 
 //Sort books from oldest to most recent
+//books.forEach((book)=> console.log(book.LatestAlbum))
 
 //sort books alphabetically
+books.sort((a, b) => a.value - b.value);
+books.sort((a, b) => {
+  const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
 
+  return 0;
+});
 //Find who wrote War and Peace
+const warnpeace = books.find((book) => book.name == "War and Peace");
+console.log(warnpeace);
 
 //how many books were written before 1900?
-
+const results = books.filter((book) => book.publishDate < 1900);
+console.log(results);
+// 4 books
 //was there at least one book published within the last 100 years?
 
 //was every book published within the last 100 years?
 
 //print a list of books that "includes" the genre historical
+//books.forEach((book) => 
+// book.genre.forEach((genre) => console.log(genre))
+//)
